7a0f17beb178c50b33651e488295fa50a2efcd4d
etnlvl <158415799+etnlvl@users.noreply.github.com>
2024-03-11 17:24:36 +1100

    update

--- transit.py
+++ transit.py
@@ -10,13 +10,13 @@ class Policy :
         self.weapon_policy = dict()
         self.surv_prob = np.zeros((n_targets, n_assignments+1))
         self.expected_value = np.zeros((n_targets, n_assignments+1))
+        self.policy_values = []
 
-
-
-    def get_first_policy(self):
+    def get_first_policy(self):                                                      ### it is useless to calculate the value of the first policy
         self.weapon_policy[f'{self.weapon_idx}'] = [0] * self.n_assignments
         for t in range (self.n_assignments) :
             self.weapon_policy[f'{self.weapon_idx}'][t] = np.random.randint(1,self.n_targets)
+
         return self.weapon_policy
 
     def initialize_surv_prob (self, first_policy, prob) :
@@ -39,28 +39,39 @@ class Policy :
         return self.surv_prob
 
 
+
+
+
     def get_next_policy(self, prob_weapon , S) :
         next_policy = []
+        policy_value = 1
         for j in range(self.n_targets) :
             for t in range (self.n_assignments) :
                 self.expected_value[j][t] = prob_weapon* S[j][t]
         print(self.expected_value)
         for t in range(self.n_assignments) :
             a = np.argmax(self.expected_value[:,t])
-            idx_to_pick_randomly = []
+            idx_to_pick_randomly = []        ### in  the case where there is several values at the same maximum expected_value, we pick randomly one of the corresponding indexes.
             for j in range (self.n_targets) :
                 if self.expected_value[j][t] == np.max(self.expected_value[:,t]) :
                     idx_to_pick_randomly.append(j)
             if a not in idx_to_pick_randomly :
                 idx_to_pick_randomly.append(a)
             next_policy.append(np.random.choice(idx_to_pick_randomly)+1)
-        return next_policy
+        print(next_policy)
+        for t in range (1,self.n_assignments) :        ## to calculate the policy_value
+            print(S)
+            print(next_policy[t])
+            policy_value = policy_value * prob_weapon * S[next_policy[t]-1][t]
+
+        return next_policy, policy_value
 
     def get_all_policies (self, n_weapons, prob_weapons) :
+
         for i in range (1, n_weapons) :
-            self.weapon_policy[f'{self.weapon_idx + i}'] = self.get_next_policy(prob_weapons[i], self.surv_prob)
-            print(self.get_next_policy(prob_weapons[i], self.surv_prob))
-            print(self.surv_prob)
+            self.weapon_policy[f'{self.weapon_idx + i}'] = self.get_next_policy(prob_weapons[i], self.surv_prob)[0]
+            print(self.get_next_policy(prob_weapons[i], self.surv_prob)[0])
+            self.policy_values.append(self.get_next_policy(prob_weapons[i], self.surv_prob)[1])
             self.update_surv_prob(prob_weapons[i],self.weapon_policy)
         return self.weapon_policy
 
@@ -73,7 +84,8 @@ print(policy)
 print(first_policy.surv_prob)
 
 all_policies = first_policy.get_all_policies(3,[0.85,0.76,0.52])
-print(all_policies)
+policy_val = first_policy.policy_values
+print(all_policies, policy_val)
 
 
 
